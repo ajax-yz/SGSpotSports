b0def904da7082547784ea736fcc8582a12effc6
ajax-chen <c.yizhao@u.nus.edu>
2018-07-24 02:09:57 +0800

    Feedback email intent done and tested

---
+++ app/src/main/java/com/example/android/sgspotsports/FeedbackFragment.java
@@ -0,0 +1,58 @@
+package com.example.android.sgspotsports;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class FeedbackFragment extends Fragment {
+
+    private EditText mEditTextSubject;
+    private EditText mEditTextMessage;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_feedback, container, false);
+
+    }
+
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        mEditTextSubject = (EditText) view.findViewById(R.id.edit_text_subject);
+        mEditTextMessage = (EditText) view.findViewById(R.id.edit_text_message);
+
+        Button buttonSend = (Button) view.findViewById(R.id.button_send);
+        buttonSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendMail();
+            }
+        });
+    }
+
+    private void sendMail(){
+        String recipient = getString(R.string.email);
+
+        String subject = mEditTextSubject.getText().toString();
+        String message = mEditTextMessage.getText().toString();
+
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, recipient);
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        emailIntent.putExtra(Intent.EXTRA_TEXT, message);
+
+        emailIntent.setType("message/rfc822");
+        startActivity(Intent.createChooser(emailIntent, "Choose an email client"));
+    }
+
+}
+
+
+

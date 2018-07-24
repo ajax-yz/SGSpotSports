e947a2fbb01be37a4dd3dda987ce5fcd409479db
ajax-chen <c.yizhao@u.nus.edu>
2018-07-24 22:48:22 +0800

    Google play services done, and Maps API key inserted

--- app/src/main/java/com/example/android/sgspotsports/LocatorFragment.java
+++ app/src/main/java/com/example/android/sgspotsports/LocatorFragment.java
@@ -1,18 +1,35 @@
 package com.example.android.sgspotsports;
 
+import android.Manifest;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 
 public class LocatorFragment extends Fragment {
+
+    private static final String TAG = "LocatorFragment";
+    private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
+    private static final String COURSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_locator, container, false);
+
+            return inflater.inflate(R.layout.fragment_locator, container, false);
+
     }
+
+
+
 }

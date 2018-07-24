000e6480e6f7b6cb3f83b535070570a538cd297c
ajax-chen <c.yizhao@u.nus.edu>
2018-07-25 02:55:31 +0800

    Before including mitch's code

--- app/src/main/java/com/example/android/sgspotsports/LocatorFragment.java
+++ app/src/main/java/com/example/android/sgspotsports/LocatorFragment.java
@@ -1,35 +1,49 @@
 package com.example.android.sgspotsports;
 
-import android.Manifest;
-import android.app.Dialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.content.ContextCompat;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class LocatorFragment extends Fragment {
+public class LocatorFragment extends Fragment implements OnMapReadyCallback {
 
-    private static final String TAG = "LocatorFragment";
-    private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
-    private static final String COURSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
+    GoogleMap map;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-            return inflater.inflate(R.layout.fragment_locator, container, false);
+        View view = inflater.inflate(R.layout.fragment_locator, container, false);
 
+        return view;
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        SupportMapFragment mapFragment = (SupportMapFragment)
+                getChildFragmentManager().findFragmentById(R.id.LocatorMap);
+        mapFragment.getMapAsync(this);
+    }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+        LatLng point = new LatLng(1.303815, 103.765892);
 
+        MarkerOptions option = new MarkerOptions();
+        option.position(point).title("West Coast Plaza");
+        map.addMarker(option);
+        map.moveCamera(CameraUpdateFactory.newLatLng(point));
+    }
 }

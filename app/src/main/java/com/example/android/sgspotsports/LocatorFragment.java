92931011c4d00adbe0883ba6b38ebcef97b1af13
ajax-chen <c.yizhao@u.nus.edu>
2018-07-25 03:31:47 +0800

    Integration is done, but marker is not showing

--- app/src/main/java/com/example/android/sgspotsports/LocatorFragment.java
+++ app/src/main/java/com/example/android/sgspotsports/LocatorFragment.java
@@ -1,12 +1,18 @@
 package com.example.android.sgspotsports;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -17,7 +23,14 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class LocatorFragment extends Fragment implements OnMapReadyCallback {
 
-    GoogleMap map;
+    private static final String TAG = "LocatorFragment";
+    private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
+    private static final String COURSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
+
+    // variables
+    private Boolean mLocationPermissionsGranted = false;
+    private GoogleMap mMap;
 
     @Nullable
     @Override
@@ -31,19 +44,78 @@ public class LocatorFragment extends Fragment implements OnMapReadyCallback {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        SupportMapFragment mapFragment = (SupportMapFragment)
-                getChildFragmentManager().findFragmentById(R.id.LocatorMap);
-        mapFragment.getMapAsync(this);
+
+        getLocationPermission();
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        map = googleMap;
+
+        Toast.makeText(getActivity(), "Map is Ready", Toast.LENGTH_SHORT).show();
+        // Or can use Toast.makeText(getContext(), "Map is Ready", Toast.LENGTH_SHORT).show();
+
+        Log.d(TAG, "onMapReady: map is ready");
+        mMap = googleMap;
         LatLng point = new LatLng(1.303815, 103.765892);
 
         MarkerOptions option = new MarkerOptions();
         option.position(point).title("West Coast Plaza");
-        map.addMarker(option);
-        map.moveCamera(CameraUpdateFactory.newLatLng(point));
+        mMap.addMarker(option);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(point));
     }
+
+    private void getLocationPermission() {
+        Log.d(TAG, "getLocationPermission: getting location permissions");
+        String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION};
+
+        if (ContextCompat.checkSelfPermission(getActivity().getApplicationContext(),
+                FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission(getActivity().getApplicationContext(),
+                    COURSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                mLocationPermissionsGranted = true;
+            } else {
+                ActivityCompat.requestPermissions(getActivity(),
+                        permissions,
+                        LOCATION_PERMISSION_REQUEST_CODE);
+            }
+        } else {
+            ActivityCompat.requestPermissions(getActivity(),
+                    permissions,
+                    LOCATION_PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    private void initMap() {
+
+        Log.d(TAG, "initMap: initializing map");
+        SupportMapFragment mapFragment = (SupportMapFragment)
+                getChildFragmentManager().findFragmentById(R.id.LocatorMap);
+
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        Log.d(TAG, "onRequestPermissionsResult: called.");
+        mLocationPermissionsGranted = false;
+
+        switch(requestCode) {
+            case LOCATION_PERMISSION_REQUEST_CODE:
+                if(grantResults.length > 0) {
+                    for (int i = 0; i < grantResults.length; i++) {
+                        if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
+                            mLocationPermissionsGranted = false;
+                            Log.d(TAG,"onRequestPermissionsResult: permission failed");
+                            return;
+                        }
+                    }
+                    Log.d(TAG,"onRequestPermissionsResult: permission granted");
+                    mLocationPermissionsGranted = true;
+                    // initialize our map
+                    initMap();
+                }
+        }
+    }
+
 }

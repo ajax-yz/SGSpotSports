058ad6f5e699530ef3f3339dc65745b1f0cd4ac7
ajax-chen <c.yizhao@u.nus.edu>
2018-07-25 19:31:59 +0800

    Current location done and tested

--- app/src/main/java/com/example/android/sgspotsports/LocatorFragment.java
+++ app/src/main/java/com/example/android/sgspotsports/LocatorFragment.java
@@ -2,6 +2,7 @@ package com.example.android.sgspotsports;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -13,13 +14,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
-
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class LocatorFragment extends Fragment implements OnMapReadyCallback {
 
@@ -27,10 +31,13 @@ public class LocatorFragment extends Fragment implements OnMapReadyCallback {
     private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
     private static final String COURSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
+    private static final float DEFAULT_ZOOM = 15f;
 
     // variables
     private Boolean mLocationPermissionsGranted = false;
     private GoogleMap mMap;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+
 
     @Nullable
     @Override
@@ -56,12 +63,30 @@ public class LocatorFragment extends Fragment implements OnMapReadyCallback {
 
         Log.d(TAG, "onMapReady: map is ready");
         mMap = googleMap;
-        LatLng point = new LatLng(1.303815, 103.765892);
 
-        MarkerOptions option = new MarkerOptions();
-        option.position(point).title("West Coast Plaza");
-        mMap.addMarker(option);
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(point));
+        if (mLocationPermissionsGranted) {
+            getDeviceLocation();
+
+            if (ActivityCompat.checkSelfPermission(getActivity().getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                    ActivityCompat.checkSelfPermission(getActivity().getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            mMap.setMyLocationEnabled(true);
+            mMap.getUiSettings().setMyLocationButtonEnabled(true);
+            mMap.getUiSettings().setMapToolbarEnabled(true);
+            mMap.getUiSettings().setAllGesturesEnabled(true);
+            mMap.getUiSettings().setZoomControlsEnabled(true);
+
+        }
+
+        /*
+         *LatLng point = new LatLng(1.303815, 103.765892);
+
+         *MarkerOptions option = new MarkerOptions();
+         *option.position(point).title("West Coast Plaza");
+         *mMap.addMarker(option);
+         *mMap.moveCamera(CameraUpdateFactory.newLatLng(point));
+         */
     }
 
     private void getLocationPermission() {
@@ -74,6 +99,7 @@ public class LocatorFragment extends Fragment implements OnMapReadyCallback {
             if (ContextCompat.checkSelfPermission(getActivity().getApplicationContext(),
                     COURSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
                 mLocationPermissionsGranted = true;
+                initMap();
             } else {
                 ActivityCompat.requestPermissions(getActivity(),
                         permissions,
@@ -84,6 +110,41 @@ public class LocatorFragment extends Fragment implements OnMapReadyCallback {
                     permissions,
                     LOCATION_PERMISSION_REQUEST_CODE);
         }
+    }
+
+
+    private void getDeviceLocation() {
+        Log.d(TAG, "getDeviceLocation: getting the device's current location");
+
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity());
+        try {
+            if (mLocationPermissionsGranted) {
+
+                Task location = mFusedLocationProviderClient.getLastLocation();
+                location.addOnCompleteListener(new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if(task.isSuccessful()){
+                            Log.d(TAG, "onComplete: found location!");
+                            Location currentLocation = (Location) task.getResult();
+                            moveCamera(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()),
+                                    DEFAULT_ZOOM);
+
+                         } else {
+                             Log.d(TAG, "onComplete: current location is null");
+                             Toast.makeText(getActivity(), "unable to get current location", Toast.LENGTH_SHORT).show();
+                         }
+                    }
+                });
+            }
+        } catch (SecurityException e) {
+            Log.e(TAG, "getDeviceLocation: SecurityException: " + e.getMessage());
+        }
+    }
+
+    private void moveCamera(LatLng latLng, float zoom) {
+        Log.d(TAG, "moveCamera: moving the camera to: lat: " + latLng.latitude + ", lng: " + latLng.longitude);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
     }
 
     private void initMap() {

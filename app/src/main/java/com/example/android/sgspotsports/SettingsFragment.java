b2386af018af20d3bda92fe30557896dbc37591d
ajax-chen <c.yizhao@u.nus.edu>
2018-08-19 02:23:57 +0800

    Crop image working but not saving to firebase

--- app/src/main/java/com/example/android/sgspotsports/SettingsFragment.java
+++ app/src/main/java/com/example/android/sgspotsports/SettingsFragment.java
@@ -1,5 +1,7 @@
 package com.example.android.sgspotsports;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -12,6 +14,8 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -19,8 +23,17 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.util.Random;
 
 import de.hdodenhof.circleimageview.CircleImageView;
+
+import static android.app.Activity.RESULT_OK;
 
 public class SettingsFragment extends Fragment {
 
@@ -36,6 +49,12 @@ public class SettingsFragment extends Fragment {
 
     private Button mStatusBtn;
     private Button mImageBtn;
+
+    private static final int GALLERY_PICK = 1;
+    private static final int MAX_LENGTH = 10;
+
+    // Storage Firebase
+    private StorageReference mImageStorage;
 
     @Nullable
     @Override
@@ -61,6 +80,8 @@ public class SettingsFragment extends Fragment {
         mCurrentUser = FirebaseAuth.getInstance().getCurrentUser();
         String current_uid = mCurrentUser.getUid();
         mUserDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(current_uid);
+
+        mImageStorage = FirebaseStorage.getInstance().getReference();
 
         mUserDatabase.addValueEventListener(new ValueEventListener() {
             @Override
@@ -96,5 +117,89 @@ public class SettingsFragment extends Fragment {
             }
         });
 
+        mImageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                Intent galleyIntent = new Intent();
+                galleyIntent.setType("image/*");
+                galleyIntent.setAction(Intent.ACTION_GET_CONTENT);
+
+                startActivityForResult(Intent.createChooser(galleyIntent, "SELECT_IMAGE"), GALLERY_PICK);
+
+
+                /* start picker to get image for cropping and then use the image in cropping activity
+                CropImage.activity()
+                        .setGuidelines(CropImageView.Guidelines.ON)
+                        .start(getActivity());
+                */
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+        if (requestCode == GALLERY_PICK && resultCode == RESULT_OK) {
+
+            Uri imageUri = data.getData();
+
+            CropImage.activity(imageUri)
+                    .setAspectRatio(1,1)
+                    .start(getActivity());
+
+            //Toast.makeText(getActivity(), (CharSequence) imageUri, Toast.LENGTH_LONG).show();
+
+        }
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+
+            if (resultCode == RESULT_OK) {
+
+                Uri resultUri = result.getUri();
+
+                StorageReference filepath = mImageStorage.child("profile_images").child(random() + ".jpg");
+
+                filepath.putFile(resultUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+
+                        if (task.isSuccessful()) {
+
+                            Toast.makeText(getActivity(), "Working", Toast.LENGTH_LONG).show();
+
+                        } else {
+
+                            Toast.makeText(getActivity(), "Error in uploading", Toast.LENGTH_LONG).show();
+
+                        }
+                    }
+                });
+
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+
+                Exception error = result.getError();
+            }
+        }
+
+    }
+
+    public static String random() {
+        Random generator = new Random();
+        StringBuilder randomStringBuilder = new StringBuilder();
+        int randomLength = generator.nextInt(MAX_LENGTH);
+        char tempChar;
+        for (int i = 0; i < randomLength; i++){
+            tempChar = (char) (generator.nextInt(96) + 32);
+            randomStringBuilder.append(tempChar);
+        }
+        return randomStringBuilder.toString();
     }
 }

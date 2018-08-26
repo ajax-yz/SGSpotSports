bfe95f5b8553990ae7b8502f80b7b874c5a680b2
ajax-chen <c.yizhao@u.nus.edu>
2018-08-26 12:18:13 +0800

    Token code added

--- app/src/main/java/com/example/android/sgspotsports/LogInFragment.java
+++ app/src/main/java/com/example/android/sgspotsports/LogInFragment.java
@@ -6,6 +6,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,9 +18,14 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
 
 public class LogInFragment extends Fragment implements View.OnClickListener {
 
@@ -30,6 +36,8 @@ public class LogInFragment extends Fragment implements View.OnClickListener {
     //private ProgressBar progressBar;
     private ProgressDialog mLoginProgress;
     private View view;
+
+    private DatabaseReference mUserDatabase;
 
     @Nullable
     @Override
@@ -58,6 +66,8 @@ public class LogInFragment extends Fragment implements View.OnClickListener {
 
         logIn = (Button) view.findViewById(R.id.buttonLogin);
         logIn.setOnClickListener(this);
+
+        mUserDatabase = FirebaseDatabase.getInstance().getReference().child("Users");
 
     }
 
@@ -103,6 +113,38 @@ public class LogInFragment extends Fragment implements View.OnClickListener {
                 if (task.isSuccessful()) {
 
                     mLoginProgress.dismiss();
+
+
+                    final String current_user_id = mAuth.getCurrentUser().getUid();
+
+                    // getToken is deprecated
+                    FirebaseInstanceId.getInstance().getInstanceId().addOnSuccessListener(new OnSuccessListener<InstanceIdResult>() {
+                        @Override
+                        public void onSuccess(InstanceIdResult instanceIdResult) {
+                            String deviceToken = instanceIdResult.getToken();
+                            Log.e("newToken",deviceToken);
+
+                            mUserDatabase.child(current_user_id).child("device_token").setValue(deviceToken)
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+
+                                            if (task.isSuccessful()) {
+
+                                                //Toast.makeText(getActivity(), "Successfully uploaded token", Toast.LENGTH_SHORT).show();
+
+                                            } else {
+
+                                                Toast.makeText(getActivity(), "Error in storing token", Toast.LENGTH_SHORT).show();
+
+                                            }
+                                        }
+                                    });
+
+                        }
+                    });
+
+
                     //finish();
                     // Changed to profile fragment for videoing
                     //SetupSetupFragment setupFragment = new SetupSetupFragment();

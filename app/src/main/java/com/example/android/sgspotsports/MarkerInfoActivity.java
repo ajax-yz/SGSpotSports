3dfe19d540dca1d9f02bc5bd211900da12cd26ba
ajax-chen <c.yizhao@u.nus.edu>
2018-09-04 02:40:48 +0800

    custom marker info page done

---
+++ app/src/main/java/com/example/android/sgspotsports/MarkerInfoActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.sgspotsports;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MarkerInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_marker_info);
+    }
+}
